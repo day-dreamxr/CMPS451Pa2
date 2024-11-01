fa802819126aa980f9aadc8b3e35cee995f78448
day-dreamxr <zachmeister2143@gmail.com>
2024-11-01 10:37:15 -0500

    Read and Plot Streams
    Reads the stream data and plots it relative to the timestamps

--- module_tmp.py
+++ module_tmp.py
@@ -1,47 +1,8 @@
 #Version: v0.1
 #Date Last Updated: 12-20-2023
 
-#%% STANDARDS   -DO NOT include this block in a new module
-'''
-Unless otherwise required, use the following guidelines
-* Style:
-    - Sort all alphabatically
-    - Write the code in aesthetically-pleasing style
-    - Names should be self-explanatory
-        - "the main variable designator_variable group name": "child_parent"
-            - pm_single, not singlepm, dataDf_grpL_1 , not dataDf_grpL1; "_1" is safer for bugs.
-    - Comment adequately.
-        - Add a comment for each code block, such as a loop-block, that describe the functionality
-     - Use relative path
-    - Use generic coding instead of manually-entered constant values
-    - Legends should be good enough in color, linestyle, shape etc. to distinguish data series.
-    - Always test your code with an artificial data whose return value is known.
-    - Add the symbol # at the end of EACH block.
- 
-* Performance and Safety:
-    - Code must be efficient (data-structure, functionality).
-    - Avoid if-block in a loop-block unless it is required.
-    - Do not calculate a common/constant value inside a loop.
-    - Avoid declarations in a loop-block unless it is required.
-    - Avoid initializing variables inside a loop unless it is required.
-    - Initialize an array if size is known.
-    - Save data in categorized folders.
-    - import only the components from a package/module to be used instead of entire one.
-
-    - Avoid using global scope
-    - Prefer to use immutable types
-    - Use deep-copy
-    - Use [None for i in Sequence] instead of [None]*len(Sequence)
-    - Initialize objects with None (null) (NOT zero) if their size is known instead of using append-like methods.
-    - Operations with dataframe
-        - Sort by the same column  name, and then reset index. As an example,
-            grid_EntrpAll = x_trans.value_counts(subset=featureLst,normalize=True)
-            reset_index().sort_values(featureLst).reset_index()
-
-'''
-
 #%% MODULE BEGINS
-module_name = '<***>'
+module_name = 'DK Crew'
 
 '''
 Version: <***>
@@ -76,7 +37,7 @@ from   copy       import deepcopy as dpcpy
 
 
 from matplotlib import pyplot as plt
-#import mne
+import mne
 import numpy  as np 
 import os
 import pandas as pd
@@ -115,7 +76,7 @@ def main():
 #%% MAIN CODE                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Main code start here
 
-with open('1_132_bk_pic.pckl', 'rb') as fp:
+with open(soi_file, 'rb') as fp:
     soi = pckl.load(fp)
 #
 
@@ -125,13 +86,35 @@ if __name__ == "__main__":
     
     print(f"\"{module_name}\" module begins.")
     
-    print(f"{soi['info']['eeg_info']['channels'][11]['label']}")
-    print(f"{soi['info']['eeg_info']['channels'][3]['label']}")
-    print(f"{soi['info']['eeg_info']['channels'][16]['label']}")
+    F8 = pd.DataFrame(soi['series'][11], soi['tStamp'])
+    F4 = pd.DataFrame(soi['series'][3], soi['tStamp'])
+    Fz = pd.DataFrame(soi['series'][16], soi['tStamp'])
+
+    fig, ax = plt.subplots(3, figsize=[15, 25])
     
-    print(f"{soi['series'][11]}")
-    plt.plot(soi['series'][11], soi['tStamp'])
+    ax[0].plot(F8)
+    ax[0].set_xlim([soi['tStamp'].min(), soi['tStamp'].max()])
+    ax[0].axhline(F8[0].mean(), color='r', linestyle='--', label='Mean')
+    ax[0].set_title('F8')
+    ax[0].legend()
+    ax[0].set_xlabel('Timestamp (µs)')
+    ax[0].set_ylabel('Signal (µV)')
     
+    ax[1].plot(F4)
+    ax[1].set_xlim([soi['tStamp'].min(), soi['tStamp'].max()])
+    ax[1].axhline(F4[0].mean(), color='r', linestyle='--', label='Mean')
+    ax[1].set_title('F4')
+    ax[1].legend()
+    ax[1].set_xlabel('Timestamp (µs)')
+    ax[1].set_ylabel('Signal (µV)')
+    
+    ax[2].plot(Fz)
+    ax[2].set_xlim([soi['tStamp'].min(), soi['tStamp'].max()])
+    ax[2].axhline(Fz[0].mean(), color='r', linestyle='--', label='Mean')
+    ax[2].set_title('Fz')
+    ax[2].legend()
+    ax[2].set_xlabel('Timestamp (µs)')
+    ax[2].set_ylabel('Signal (µV)')
     
     #TEST Code
     main()
